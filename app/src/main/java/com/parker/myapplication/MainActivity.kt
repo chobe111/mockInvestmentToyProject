4c7095d2662d11c2f33b335ad2c4189adc8e75a9
chobe111 <chobe1@naver.com>
2021-01-26 21:32:31 +0900

    Update MainActivity.kt

--- app/src/main/java/com/parker/myapplication/MainActivity.kt
+++ app/src/main/java/com/parker/myapplication/MainActivity.kt
@@ -2,14 +2,19 @@ package com.parker.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.parker.myapplication.databinding.ActivityMainBinding
+import com.parker.myapplication.page.LoginFragment
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
-    private fun changeToLoginFragment() {
-
+    private val loginFragment by lazy { LoginFragment() }
+    
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment)
+            .commit()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +23,6 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        changeToLoginFragment()
+        changeFragment(loginFragment)
     }
 }
