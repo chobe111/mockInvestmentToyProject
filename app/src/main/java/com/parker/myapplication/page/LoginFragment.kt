a13aeac479fe5a936afb7633f2f0a491836f62ce
chobe111 <chobe1@naver.com>
2021-01-26 21:32:25 +0900

    Update LoginFragment.kt

--- app/src/main/java/com/parker/myapplication/page/LoginFragment.kt
+++ app/src/main/java/com/parker/myapplication/page/LoginFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.parker.myapplication.R
+import com.parker.myapplication.databinding.FragmentLoginBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,12 +20,17 @@ import com.parker.myapplication.R
 class LoginFragment : Fragment() {
     // TODO: Rename and change types of parameters
 
-
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+    
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        val view = binding.root
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+
+        return view
     }
 }
