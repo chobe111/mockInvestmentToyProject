118f186bb965e53f28516f10b9412096732c1acd
chobe111 <chobe1@naver.com>
2021-01-26 21:03:42 +0900

    Update MainActivity.kt

--- app/src/main/java/com/parker/myapplication/MainActivity.kt
+++ app/src/main/java/com/parker/myapplication/MainActivity.kt
@@ -2,10 +2,22 @@ package com.parker.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.parker.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private fun changeToLoginFragment() {
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        changeToLoginFragment()
     }
 }
