7749e9d2d1b7c4f897fec7a21cff71719fb57c97
chobe111 <chobe1@naver.com>
2021-02-28 22:28:30 +0900

    Add Clid Fragment of Invest Fragment

---
+++ app/src/main/java/com/parker/myapplication/helper/adapter/StockListAdapter.kt
@@ -0,0 +1,52 @@
+package com.parker.myapplication.helper.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import com.parker.myapplication.R
+import com.parker.myapplication.data.StockInfo
+import com.parker.myapplication.databinding.StockCardViewItemBinding
+
+class StockListAdapter(val context: Context, val stockInfo: ArrayList<StockInfo>) : BaseAdapter() {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+//      TODO: Consider View Holder
+        val view: View =
+            LayoutInflater.from(context).inflate(R.layout.stock_card_view_item, parent, false)
+
+        val stockInfo: StockInfo = getItem(position)
+        initView(view, stockInfo)
+        return view
+    }
+
+    private fun isPriceUp(): Boolean {
+        return false
+    }
+
+    private fun initView(view: View, stockInfo: StockInfo) {
+        val stockItem: TextView = view.findViewById(R.id.stock_item)
+        val gapRate: TextView = view.findViewById(R.id.gap_rate)
+        val price: TextView = view.findViewById(R.id.current_price)
+        val gapPrice: TextView = view.findViewById(R.id.gap_price)
+
+        stockItem.text = stockInfo.name
+        gapRate.text = stockInfo.gapRate
+        price.text = stockInfo.currentPrice
+        gapPrice.text = stockInfo.gapPrice
+    }
+
+    override fun getCount(): Int {
+        return stockInfo.size
+    }
+
+    override fun getItem(position: Int): StockInfo {
+        return stockInfo[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+}
---
+++ app/src/main/java/com/parker/myapplication/page/investment/DomesticInvestmentPage.kt
@@ -0,0 +1,65 @@
+package com.parker.myapplication.page.investment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ListView
+import androidx.databinding.DataBindingUtil
+import com.parker.myapplication.R
+import com.parker.myapplication.data.StockInfo
+import com.parker.myapplication.databinding.FragmentDomesticInvestmentPageBinding
+import com.parker.myapplication.helper.adapter.StockListAdapter
+
+class DomesticInvestmentPage : Fragment() {
+
+    private lateinit var binding: FragmentDomesticInvestmentPageBinding
+    private lateinit var listView: ListView
+
+
+    private fun setListViewAdapter() {
+        listView.adapter = StockListAdapter(requireContext(), InvestmentFragment.stockList)
+    }
+
+    private fun setVariables() {
+        listView = binding.stockListView
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_domestic_investment_page,
+            container,
+            false
+        )
+        val view = binding.root
+        return view
+
+    }
+
+    companion object {
+        val stockList: ArrayList<StockInfo> = arrayListOf(
+            StockInfo("삼성전자", "288,000", "2800", "-1.78"),
+            StockInfo("삼성전자", "288,000", "2800", "-1.78"),
+            StockInfo("삼성전자", "288,000", "2800", "-1.78"),
+            StockInfo("삼성전자", "288,000", "2800", "-1.78"),
+            StockInfo("삼성전자", "288,000", "2800", "-1.78"),
+            StockInfo("삼성전자", "288,000", "2800", "-1.78"),
+            StockInfo("삼성전자", "288,000", "2800", "-1.78"),
+            StockInfo("삼성전자", "288,000", "2800", "-1.78"),
+            StockInfo("삼성전자", "288,000", "2800", "-1.78"),
+            StockInfo("삼성전자", "288,000", "2800", "-1.78"),
+            StockInfo("삼성전자", "288,000", "2800", "-1.78"),
+            StockInfo("삼성전자", "288,000", "2800", "-1.78"),
+            StockInfo("삼성전자", "288,000", "2800", "-1.78"),
+            StockInfo("삼성전자", "288,000", "2800", "-1.78"),
+            StockInfo("삼성전자", "288,000", "2800", "-1.78"),
+            StockInfo("삼성전자", "288,000", "2800", "-1.78"),
+            StockInfo("삼성전자", "288,000", "2800", "-1.78")
+        )
+    }
+}
