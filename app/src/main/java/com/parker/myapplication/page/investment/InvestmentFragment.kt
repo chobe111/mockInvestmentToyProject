f796dffc36cc1c2f7fd91eb0ca2a852526c6e811
chobe111 <chobe1@naver.com>
2021-02-28 19:10:13 +0900

    Create Investment Fragment

---
+++ app/src/main/java/com/parker/myapplication/page/investment/InvestmentFragment.kt
@@ -0,0 +1,28 @@
+package com.parker.myapplication.page.investment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.parker.myapplication.R
+import com.parker.myapplication.databinding.FragmentInvestmentBinding
+
+// TODO: Rename parameter arguments, choose names that match
+
+class InvestmentFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+
+    private lateinit var binding: FragmentInvestmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_investment, container, false)
+        val view = binding.root
+        return view
+
+    }
+}
