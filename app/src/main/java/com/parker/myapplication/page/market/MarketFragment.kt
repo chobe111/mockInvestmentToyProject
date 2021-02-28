20d55b7599be077998d85019dd9f008e8496871e
chobe111 <chobe1@naver.com>
2021-03-01 03:55:33 +0900

    Update MarketFragment.kt

--- app/src/main/java/com/parker/myapplication/page/market/MarketFragment.kt
+++ app/src/main/java/com/parker/myapplication/page/market/MarketFragment.kt
@@ -1,13 +1,18 @@
 package com.parker.myapplication.page.market
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.parker.myapplication.R
+import com.parker.myapplication.data.StockInfo
 import com.parker.myapplication.databinding.FragmentMarketBinding
+import com.parker.myapplication.helper.TAG
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 
@@ -24,8 +29,42 @@ class MarketFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_market, container, false)
         val view = binding.root
 
-        val doc: Document = Jsoup.connect("https://m.stock.naver.com/sise/siseList.nhn?menu=market_sum&sosok=0").get()
-        val wholeStockData = doc.select("tbody[class=_list_wrap]")
+        GlobalScope.launch {
+            val datalist: MutableList<StockInfo> = arrayListOf()
+
+            val doc: Document =
+                Jsoup.connect("https://finance.naver.com/sise/sise_market_sum.nhn?&page=1").get()
+            val wholeStockData = doc.select("table[class=type_2]").select("tbody").select("tr")
+            Log.d(TAG, "onCreateView: asdfadfadf")
+            for (data in wholeStockData) {
+                val tdLen = data.select("td").size
+                if (tdLen <= 5) continue
+                val tdList = data.select("td")
+                var name = ""
+                var price = ""
+                var priceGap = ""
+                var gap = ""
+                tdList.forEachIndexed { index, element ->
+                    when(index){
+                        1 -> {
+                            name = element.select("a").text()
+                        }
+                        2 -> {
+                            price = element.text()
+                        }
+                        3 -> {
+                            priceGap = element.text()
+                        }
+                        4 -> {
+                            gap = element.text()
+                        }
+                    }
+                }
+                datalist.add(StockInfo(name, price, priceGap, gap))
+            }
+        }
+
+
 
         return view
     }
