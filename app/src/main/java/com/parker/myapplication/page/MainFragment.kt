bd4a0f711588e32f5b5081f7ed194d94972f3f80
chobe111 <chobe1@naver.com>
2021-03-01 06:03:43 +0900

    Update MainFragment.kt

--- app/src/main/java/com/parker/myapplication/page/MainFragment.kt
+++ app/src/main/java/com/parker/myapplication/page/MainFragment.kt
@@ -35,7 +35,7 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main2, container, false)
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
     companion object {
