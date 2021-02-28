c695a4eab1603f745b8af9a9125793d42f0236bc
chobe111 <chobe1@naver.com>
2021-03-01 02:28:16 +0900

    Add Coroutine in Gradle

--- app/src/main/java/com/parker/myapplication/page/market/MarketFragment.kt
+++ app/src/main/java/com/parker/myapplication/page/market/MarketFragment.kt
@@ -8,6 +8,8 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.parker.myapplication.R
 import com.parker.myapplication.databinding.FragmentMarketBinding
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 
 // TODO: Rename parameter arguments, choose names that match
 
@@ -21,7 +23,10 @@ class MarketFragment : Fragment() {
     ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_market, container, false)
         val view = binding.root
+
+        val doc: Document = Jsoup.connect("https://m.stock.naver.com/sise/siseList.nhn?menu=market_sum&sosok=0").get()
+        val wholeStockData = doc.select("tbody[class=_list_wrap]")
+
         return view
     }
-
 }
